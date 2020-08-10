930f53d5ce9a8d0472b05cec22bf39102e79e7c6
Rahul V Ramesh <rahulvramesh@hotmail.com>
2020-08-10 18:17:20 +0530

    added dep

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-exports.printMsg = function () {
-  console.log("This is a message from the demo package");
+import Cookies from "js-cookie";
+
+let sharedToken = [
+  "authToken",
+  "ghauthcode",
+  "defaultVendor",
+  "authRole",
+  "authName",
+  "authEmail",
+];
+
+let cookies = {
+  set: (key, value, options) => {
+    //check if the key in array
+    if (sharedToken.includes(key)) {
+      //set for all domain
+      if (process.env.NODE_ENV === "production") {
+        Cookies.set(key, value, {
+          expires: options.expires ? options.expires : 1,
+          domain: "greenhouse.co",
+          secure: true,
+          sameSite: "strict",
+        });
+        Cookies.set(key, value, {
+          expires: options.expires ? options.expires : 1,
+          domain: "vendor.greenhouse.co",
+          secure: true,
+          sameSite: "strict",
+        });
+      } else {
+        Cookies.set(key, value, {
+          expires: options.expires ? options.expires : 1,
+          domain: "staging.greenhouse.co",
+          secure: true,
+          sameSite: "strict",
+        });
+        Cookies.set(key, value, {
+          expires: options.expires ? options.expires : 1,
+          domain: "dev-vendor.greenhouse.co",
+          secure: true,
+          sameSite: "strict",
+        });
+      }
+    } else {
+      Cookies.set(key, value, options);
+    }
+  },
 };
+
+export let Cookies = cookies;
